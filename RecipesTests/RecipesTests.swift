464fc124269fc4f1a88587849301ed30ddcc3993
Jónódón Ó’Coileáin <jontwlc@gmail.com>
2025-05-29 21:06:20 -0400

    Adds a test

--- RecipesTests/RecipesTests.swift
+++ RecipesTests/RecipesTests.swift
@@ -6,11 +6,19 @@
 //
 
 import Testing
+import XCTest
 @testable import Recipes
 
 struct RecipesTests {
 
     @Test func example() async throws {
+        let dataModel = RecipesDataModel()
+        do {
+            let data = try await dataModel.requestRecipes()
+            XCTAssertTrue(data.count > 0)
+        } catch {
+            print(error)
+        }
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
